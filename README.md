82142d950ec27659e6868c9e7beb1853a79dfb0f
Josh Starcher <josh.starcher@gmail.com>
2017-10-09 15:51:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# node-lambda-template
+
+A node.js lambda script for extracting adobe analytics clickstreams for
+further processing.
+
+It uses [node-lambda](https://github.com/motdotla/node-lambda) under the hood to locally run and also deploy your node.js Amazon Lambda application.
+
+## Install
+
+Clone this repository.
+
+```
+cd /to/your/template/path
+npm install
+```
+
+## Usage
+
+There are 4 available commands to use on this template. For more info and usage descriptions, see the [node-lambda](https://github.com/motdotla/node-lambda) repository.
+
+```
+cd /to/your/template/path
+npm run setup # setup node-lambda files
+npm run test # test your event handler and check output
+npm run package # just generate the zip that would be uploaded to AWS
+npm run deploy # deploy to AWS
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+console.log('Loading function');
+
+const aws = require('aws-sdk');
+
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+const fs = require('fs');
+const unzipper = require('unzipper');
+const UploadStream = require("s3-stream-upload");
+
+exports.handler = function(event, context, callback) {
+    // Get the object from the event and show its content type
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const params = {
+        Bucket: bucket,
+        Key: key,
+    };
+    unzipper.Open.s3(s3, params)
+      .then(function(d) {
+        console.log('directory',d);
+        return new Promise(function(resolve, reject) {
+          d.files.forEach(function(file) {
+            file.stream()
+              .pipe(UploadStream(s3, { Bucket: bucket, Key: key.replace('cruproduction/', 'cruproduction/unzipped/').replace('.zip','/') + file.path }))
+              .on('error',reject)
+              .on('finish',resolve)
+          });
+        });
+      });
+};
