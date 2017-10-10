da4bdda5f58c018e78fdd655b8eeefabba4479c4
Josh Starcher <josh.starcher@gmail.com>
2017-10-09 21:02:35 -0400

    Convert from tsv to csv.
    Only process hit_data.tsv.

--- index.js
+++ index.js
@@ -9,25 +9,29 @@ const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 const fs = require('fs');
 const unzipper = require('unzipper');
 const upload_stream = require("s3-stream-upload");
+const csv = require('csv');
 
 exports.handler = function(event, context, callback) {
-    // Get the object from the event and show its content type
+    // Get the object from the event
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     const params = {
         Bucket: bucket,
         Key: key,
     };
+    const parser = csv.parse({delimiter: '\t', relax: true})
+
     unzipper.Open.s3(s3, params)
       .then(function(d) {
         console.log('directory',d);
-        return new Promise(function(resolve, reject) {
-          d.files.forEach(function(file) {
+        d.files.forEach(function(file) {
+          if (file.path == 'hit_data.tsv') {
+            var new_path = file.path.replace('.tsv', '.csv')
             file.stream()
-              .pipe(upload_stream(s3, { Bucket: bucket, Key: key.replace('cruproduction/', 'cruproduction/unzipped/').replace('.zip','/') + file.path }))
-              .on('error',reject)
-              .on('finish',resolve)
-          });
+              .pipe(parser)
+              .pipe(csv.stringify())
+              .pipe(upload_stream(s3, { Bucket: bucket, Key: key.replace('cruproduction/', 'cruproduction/unzipped/').replace('.zip','/') + new_path }))
+          }
         });
       });
 };
